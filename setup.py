da1509aeef96d9809e903abe313517d726eac4e7
Brian Cohan <briancohan@gmail.com>
2020-06-22 23:45:09 -0400

    install requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+dev_req = [
+]
+
+test_req = [
+    'pytest>=5.4.3',
+]
+
 setuptools.setup(
     name="pyrva_talk",
     version="0.0.1",
@@ -13,6 +20,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pyrva/rvatech-2020-07-21/tree/twitter_data",
     package_dir={"": "src"},
+    install_requires=[
+        'python-dotenv>=0.13.0',
+        'streamlit>=0.61.0',
+        'tweepy>=3.8.0',
+    ],
+    extras_require={
+        'dev': test_req + dev_req,
+        'test': test_req,
+    },
     packages=setuptools.find_packages("src"),
     classifiers=[
         "Programming Language :: Python :: 3",
