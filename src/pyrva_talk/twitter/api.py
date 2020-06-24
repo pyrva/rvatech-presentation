9e86987b2aeb1179d57064bd96f389f4c8d9db07
stephen lowery <slvt16@vt.edu>
2020-06-24 18:03:41 -0400

    fixed api issues, implemented time based query

--- src/pyrva_talk/twitter/api.py
+++ src/pyrva_talk/twitter/api.py
@@ -1,5 +1,6 @@
 """ Access to the twitter API """
 import datetime as dt
+from itertools import chain
 
 import tweepy
 
@@ -7,46 +8,34 @@ from pyrva_talk.twitter.auth import get_auth
 
 
 auth = get_auth()
-api = tweepy.API(auth)
-py_rva = api.get_user("@PyRVA")
+twitter_api = tweepy.API(auth)
+py_rva = "@PyRVA"
 
 
 def get_timeline(user=py_rva):
     """ Returns timeline for ``user`` """
-    timeline = api.user_timeline(user)
+    timeline = twitter_api.user_timeline(user)
     return timeline
 
 
 def get_recent_mentions(user=py_rva, n=5):
     """ Get the ``n`` most recet mentions for ``user`` """
-    tweets = api.mentions_timeline(user, count=n)
+    tweet_cursor = tweepy.Cursor(twitter_api.mentions_timeline, user=user, count=n)
+    tweets = [tweet for tweet in chain.from_iterable(tweet_cursor.pages())]
     return tweets
 
 
-def get_tweets_by_hour(query='#PyRVA', num_hours=12):
-    """ Count tweets for ``query`` per hour for the last ``num_hours`` hours """
-    tweets_by_time = {}
+def get_tweets(query=py_rva, start_date=None):
+    """ Return tweets for ``query`` (since datetime ``since``)"""
 
-    # put last 12 hours as empty lists in ``tweets_by_time``
-    now = dt.datetime.now()
-    now = dt.datetime(now.year, now.month, now.day, now.hour)
-    for i in range(num_hours):
-        time = now - dt.timedelta(hours=i)
-        tweets_by_time[time] = []
+    payload = {
+        'q': query
+    }
+    if start_date:
+        payload['since'] = start_date
 
     # get tweets
-    tweets = api.search(query)
-
-    # bucket tweets
-    for tweet in tweets:
-        tweet_time = dt.datetime(
-            tweet.created_at.year,
-            tweet.created_at.month,
-            tweet.created_at.day,
-            tweet.created_at.hour,
-        )
-        if tweet_time in tweets_by_time:
-            tweets_by_time[tweet_time].append(tweet)
+    tweet_cursor = tweepy.Cursor(twitter_api.search, **payload)
+    tweets = [tweet for tweet in chain.from_iterable(tweet_cursor.pages())]
     
-    return tweets_by_time
-
+    return tweets
--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -1,5 +1,6 @@
-import pytest
+import datetime as dt
 
+import pytest
 
 
 def test_twitter_auth():
@@ -10,3 +11,34 @@ def test_twitter_auth():
     auth = get_auth()
     api = API(auth)
     assert api.verify_credentials()
+
+
+def test_get_timeline():
+    from pyrva_talk.twitter.api import get_timeline
+
+    tweets = get_timeline()
+
+    assert isinstance(tweets, list)
+
+
+def test_get_recent_mentions():
+    from pyrva_talk.twitter.api import get_recent_mentions
+
+    tweets = get_recent_mentions()
+
+    assert isinstance(tweets, list)
+
+
+def test_get_tweets():
+    from pyrva_talk.twitter.api import get_tweets
+
+    tweets = get_tweets()
+    assert isinstance(tweets, list)
+
+
+def test_get_tweets_since_yesterday():
+    from pyrva_talk.twitter.api import get_tweets
+
+    yesterday = dt.datetime.now() - dt.timedelta(days=1)
+    tweets = get_tweets(start_date=yesterday.strftime("%x"))
+    assert isinstance(tweets, list)
